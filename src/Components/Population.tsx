c320442d7c548337a31607624ecc10feecb6de19
jerinwilson7 <jerinwilson7@gmail.com>
2024-07-10 09:46:24 +0530

    endPoint-updated

--- src/Components/Population.tsx
+++ src/Components/Population.tsx
@@ -9,7 +9,7 @@ type PopulationPropsType = {
 export const Population = ({populations}:PopulationPropsType) => {
     return (
       <div className="bg-forcast px-6 pt-8 pb-4 rounded-2xl">
-              <div className="flex overflow-scroll gap-16 justify-between items-center w-full text-white">
+              <div className="flex overflow-scroll gap-16 items-center w-full text-white">
   
                  {
                     populations.data.map((population,index)=>(
